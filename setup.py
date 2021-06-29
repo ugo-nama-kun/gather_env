07ecc59bea1fbbc18c2e3f216c93e1440b13156c
Naoto Yoshida <y_naoto@isi.imi.i.u-tokyo.ac.jp>
2021-06-30 03:04:10 +0900

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='Gather',
-      version='0.0.1',
+      version='0.1.0',
       install_requires=['gym', 'mujoco-py']  # And any other dependencies foo needs
 )
